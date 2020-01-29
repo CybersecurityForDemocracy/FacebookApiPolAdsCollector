033d045976262660a470fc9ce0e9de8f33a55090
Paul Duke <mountainmanduke@gmail.com>
2020-01-29 15:47:47 -0500

    add migration of impressions for ad batches

--- schema_migration_20200129/schema_migrator.py
+++ schema_migration_20200129/schema_migrator.py
@@ -154,7 +154,7 @@ class SchemaMigrator:
     src_cursor = self.get_src_cursor()
     src_cursor.array_size = self.batch_size
     src_pages_query = 'SELECT (page_id, page_name) from pages'
-    srcs_cursor.execute(src_cursor.mogrify(src_page_query))
+    srcs_cursor.execute(src_cursor.mogrify(src_pages_query))
     fetched_rows = src_cursor.fetchmany()
     num_rows_processed = 0
     while fetched_row:
@@ -169,7 +169,7 @@ class SchemaMigrator:
 
     logging.info('Migrated %d page rows total.', num_rows_processed)
 
-  def migrate_ads_table(self):
+  def migrate_ads_and_impressions_table(self):
     logging.info('Migrationg ads table.')
     src_cursor = self.get_src_cursor()
     src_cursor.array_size = self.batch_size
@@ -179,7 +179,9 @@ class SchemaMigrator:
     num_rows_processed = 0
     while fetched_rows:
       ad_records = []
+      ad_status = {}
       for row in fetched_rows:
+        ad_status[row['arcive_id']] = row['ad_status']
         old_ad_record = OldAdRecord(row)
         ad_records.apeend(NewAdRecord(
         ad_creation_time=old_ad_record.creation_date,
@@ -201,67 +203,44 @@ class SchemaMigrator:
         publisher_platform=None))
 
       self.dest_db_interface.dest_db_interface.insert_new_ads(ad_records)
+      self.migrate_impressions_for_archive_id_batch(ad_status)
       num_rows_processed += len(ad_records)
       logging.info('Migrated %d page rows so far.', num_rows_processed)
 
     logging.info('Migrated %d page rows total.', num_rows_processed)
 
-  def migrate_ads_table(self):
-    logging.info('Migrationg ads table.')
+  def migrate_impressions_for_archive_id_batch(self, archive_id_to_ad_status):
+    logging.info('Migrationg impressions table.')
     src_cursor = self.get_src_cursor()
     src_cursor.array_size = self.batch_size
-    srcs_ads_query = 'SELECT * FROM ads;'
-    srcs_cursor.execute(src_cursor.mogrify(src_ads_query))
-    fetched_rows = src_cursor.fetchmany()
+    archive_ids = [str(k) for k in archive_id_to_ad_status]
+    src_impressions_query = 'SELECT * FROM impressions WHERE archive_id IN '
+    '(%s)' % (','.join(archive_ids))
+    srcs_cursor.execute(src_cursor.mogrify(src_impressions_query))
+    fetched_rows = src_cursor.fetchall()
     num_rows_processed = 0
-    while fetched_rows:
-      ad_records = []
-      for row in fetched_rows:
-        old_ad_record = OldAdRecord(row)
-        ad_records.apeend(NewAdRecord(
-        ad_creation_time=old_ad_record.creation_date,
-        ad_creative_body=old_ad_record.text,
-        ad_creative_link_caption=old_ad_record.ad_creative_link_caption,
-        ad_creative_link_description=old_ad_record.ad_creative_link_description,
-        ad_creative_link_title=old_ad_record.ad_creative_link_title,
-        ad_delivery_start_time=old_ad_record.start_date,
-        ad_delivery_stop_time=old_ad_record.end_date,
-        ad_snapshot_url=row['snapshot_url'],
-        ad_status=old_ad_record.is_active,
-        archive_id=old_ad_record.archive_id,
-        country_code='US',
-        currency='USD',
-        first_crawl_time=None,
-        funding_entity=old_ad_record.roe['funding_entity'],
-        page_id=old_ad_record.page_id,
-        page_name=old_ad_record.page_name,
-        publisher_platform=None))
+    impression_records = []
+    archive_ids_in_results = set()
+    for row in fetched_row:
+      archive_id = row['archive_id']
+      archive_ids_in_results.add(archive_id)
+      impression_records.append(NewAdRecord(archive_id=archive_id,
+        ad_status=archive_id_to_ad_status[archive_id],
+        impressions__lower_bound=row['min_impressions'],
+        impressions__upper_bound=row['max_impressions'],
+        spend__lower_bound=row['min_spend'],
+        spend__upper_bound=row['max_spend']))
 
-      self.dest_db_interface.dest_db_interface.insert_new_ads(ad_records)
-      num_rows_processed += len(ad_records)
-      logging.info('Migrated %d page rows so far.', num_rows_processed)
+    archive_ids_to_fetch = set(archive_id_to_ad_status.keys())
+    if archive_ids_in_results != archive_ids_to_fetch:
+      logging.error('Did not get impressions for archive IDs: %s. IMPRESSIONS '
+          'TABLE MIGRATION MAY BE INCOMPLETE.',
+          archive_ids_to_fetch.difference(archive_ids_in_results))
 
-    logging.info('Migrated %d page rows total.', num_rows_processed)
-
-  def migrate_impressions_table(self):
-    logging.info('Migrationg pages table.')
-    src_cursor = self.get_src_cursor()
-    src_cursor.array_size = self.batch_size
-    src_pages_query = 'SELECT (page_id, page_name) from pages'
-    srcs_cursor.execute(src_cursor.mogrify(src_page_query))
-    fetched_rows = src_cursor.fetchmany()
-    num_rows_processed = 0
-    while fetched_row:
-      page_records = []
-      for row in fetched_row:
-        page_records.append(NewPageRecord(page_id=row['page_id'],
-          page_name=row['page_name']))
-
-      self.dest_db_interface.dest_db_interface.insert_pages(page_records)
-      num_rows_processed += len(page_records)
-      logging.info('Migrated %d page rows so far.', num_rows_processed)
-
-    logging.info('Migrated %d page rows total.', num_rows_processed)
+    self.dest_db_interface.dest_db_interface.insert_impressions(impression_records)
+    num_rows_processed += len(impression_records)
+    logging.info('Migrated %d impression rows for %d archive IDs.',
+        num_rows_processed, len(archive_ids))
 
 def main(src_db_connection, dest_db_connection, batch_size=1000):
   schema_migrator = SchemaMigraton(src_db_connection, dest_db_connection,
