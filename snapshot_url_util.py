8b945ef7422cb04d1900b63e0f44fb6ef82741e1
Paul Duke <mountainmanduke@gmail.com>
2020-02-11 17:51:47 -0500

    2 version of snapshot URL util. one to make flat list, other to make map archive ID -> snapshot URL

--- snapshot_url_util.py
+++ snapshot_url_util.py
@@ -14,6 +14,26 @@ def construct_snapshot_urls(access_token, archive_ids):
     Returns:
         list (str) of snapshot URLs with archive ID and access token.
     """
+    snapshot_urls = []
+    for archive_id in archive_ids:
+        url = '%s?%s' % (FB_AD_SNAPSHOT_BASE_URL,
+                         urllib.parse.urlencode({
+                             'id': archive_id,
+                             'access_token': access_token
+                         }))
+        logging.debug('Constructed snapshot URL %s', url)
+        snapshot_urls.append(url)
+    return snapshot_urls
+
+def construct_archive_id_to_snapshot_url_map(access_token, archive_ids):
+    """Get map of archive ID -> snapshot URL for the given archive_ids and access_token,
+
+    Args:
+        access_token: API access_token to use for URL.
+        archive_ids: list of archive IDs make snapshot URLs for.
+    Returns:
+        Dict archive ID -> str snapshot URL for ID with given access token.
+    """
     archive_id_to_snapshot_url = {}
     for archive_id in archive_ids:
         url = '%s?%s' % (FB_AD_SNAPSHOT_BASE_URL,
