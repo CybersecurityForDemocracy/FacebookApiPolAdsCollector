a0d174355c977732a6463133bb0e19db2bf1f560
Paul Duke <mountainmanduke@gmail.com>
2020-02-18 11:51:28 -0500

    formatting fixes

--- standard_logger_config.py
+++ standard_logger_config.py
@@ -1,8 +1,12 @@
 import logging
 
-def ConfigureLogger(log_filename, include_date=True):
+
+def ConfigureLogger(log_filename):
+    record_format = (
+        '[%(levelname)s\t%(asctime)s] %(process)d %(thread)d {%(filename)s:%(lineno)d} '
+        '%(message)s')
     logging.basicConfig(
-            handlers=[logging.FileHandler(log_filename), logging.StreamHandler()],
-            format=('[%(levelname)s\t%(asctime)s] %(process)d %(thread)d {%(filename)s:%(lineno)d} '
-                    '%(message)s'),
-            level=logging.INFO)
+        handlers=[logging.FileHandler(log_filename),
+                  logging.StreamHandler()],
+        format=record_format,
+        level=logging.INFO)
