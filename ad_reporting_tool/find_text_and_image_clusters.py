d3b372f32eafd7f8ec7eb578f69e708ffa88f868
Paul Duke <mountainmanduke@gmail.com>
2020-02-25 09:22:00 -0500

    rename get_snapshot_urls_for_random_duplicate_simhash.py -> find_text_and_image_clusters.py, and also dump text clusters as JSON

--- ad_reporting_tool/find_text_and_image_clusters.py
+++ ad_reporting_tool/find_text_and_image_clusters.py
@@ -29,13 +29,18 @@ def main(config):
     db_connection = get_db_connection(config)
     logging.info('DB connection %s', db_connection.dsn)
 
-    archive_ids_with_similar_text = text_clustering_utils.ad_creative_body_text_similarity_clusters(db_connection)
+    text_simhash_clusters = text_clustering_utils.ad_creative_body_text_similarity_clusters(db_connection)
     logging.info('First 3 sets of archive IDs with similar text: %s',
-                 archive_ids_with_similar_text[:3])
-    random_archive_id_set = random.choice(archive_ids_with_similar_text)
+                 text_simhash_clusters[:3])
+    random_archive_id_set = random.choice(text_simhash_clusters)
     while len(random_archive_id_set) == 1:
-        random_archive_id_set = random.choice(archive_ids_with_similar_text)
+        random_archive_id_set = random.choice(text_simhash_clusters)
     logging.info('Archive IDs %s have similar text simhashes', random_archive_id_set)
+    text_simhash_clusters_as_lists = [list(cluster) for cluster in text_simhash_clusters]
+    text_clusters_filename = 'text_clusters.json'
+    with open(text_clusters_filename, 'w') as f:
+        json.dump(text_simhash_clusters_as_lists, f)
+    print(f'Wrote text simhash clusters as JSON to {text_clusters_filename}')
 
     image_simhash_clusters = text_clustering_utils.ad_creative_image_similarity_clusters(db_connection)
     logging.info('First 3 sets of archive IDs with similar image: %s',
