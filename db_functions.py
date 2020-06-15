720f97c96e07b790c7660910971a856ebc06e14d
Paul Duke <mountainmanduke@gmail.com>
2020-06-15 12:48:50 -0400

    fix insert template syntax

--- db_functions.py
+++ db_functions.py
@@ -401,7 +401,7 @@ class DBInterface():
             '(archive_id, text_sha256_hash, image_sha256_hash) DO NOTHING')
         insert_template = (
             '(%(archive_id)s, %(ad_creative_body)s, %(ad_creative_link_url)s, '
-            '%(ad_creative_body_language)s %(ad_creative_link_title)s, '
+            '%(ad_creative_body_language)s, %(ad_creative_link_title)s, '
             '%(ad_creative_link_caption)s, %(ad_creative_link_description)s, %(text_sha256_hash)s, '
             '%(text_sim_hash)s, %(image_downloaded_url)s, %(image_bucket_path)s, '
             '%(image_sim_hash)s, %(image_sha256_hash)s)')
