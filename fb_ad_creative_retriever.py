34780a653518e3fea5d1dc01ad93e84265230065
Paul Duke <mountainmanduke@gmail.com>
2020-06-15 12:47:44 -0400

    syntax fix

--- fb_ad_creative_retriever.py
+++ fb_ad_creative_retriever.py
@@ -777,7 +777,7 @@ class FacebookAdCreativeRetriever:
                 text_sha256_hash = hashlib.sha256(bytes(
                     text, encoding='UTF-32')).hexdigest()
                 try:
-                ad_creative_body_language = detect(creative.creative_body)
+                    ad_creative_body_language = detect(creative.creative_body)
                 except langdetect.lang_detect_exception.LangDetectException as error:
                     logging.info('Unable to determine language of %s ad creative body: %s',
                                  creative.archive_id, creative.creative_body)
