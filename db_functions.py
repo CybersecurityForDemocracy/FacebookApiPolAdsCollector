fd0be82fc2c0ccce97b4e820b2d270f7d5dc3619
Paul Duke <mountainmanduke@gmail.com>
2020-06-19 13:14:41 -0400

    close list comprehension

--- db_functions.py
+++ db_functions.py
@@ -806,9 +806,9 @@ class DBInterface():
     def update_ad_last_active_date(self, archive_ids):
         cursor = self.get_cursor()
         update_last_active_field_query = (
-             'UPDATE impressions set last_active_date = CURRENT_DATE WHERE archive_id = %s')
+            'UPDATE impressions set last_active_date = CURRENT_DATE WHERE archive_id = %s')
         # execute_batch requires an interable of iterables for arglist
-        archive_id_arg_list = [tuple(archive_id) for archive_id in archive_ids,
+        archive_id_arg_list = [tuple(archive_id) for archive_id in archive_ids]
         psycopg2.extras.execute_batch(cursor,
                                       archive_id_arg_list,
                                       update_last_active_field_query,
