a602e719665066bfc61715f7e0effe06f84224e4
Paul Duke <mountainmanduke@gmail.com>
2020-08-05 16:33:54 -0400

    use CURRENT_DATE for deprecated_page_names.deprecated_on since it is a date type

--- sql/unified_schema.sql
+++ sql/unified_schema.sql
@@ -103,7 +103,7 @@ CREATE TABLE page_metadata (
 CREATE TABLE deprecated_page_names (
   page_id bigint NOT NULL,
   page_name character varying NOT NULL,
-  deprecated_on date DEFAULT CURRENT_TIMESTAMP NOT NULL,
+  deprecated_on date DEFAULT CURRENT_DATE NOT NULL,
   last_modified_time timestamp with time zone DEFAULT CURRENT_TIMESTAMP NOT NULL,
   CONSTRAINT page_id_fk FOREIGN KEY (page_id) REFERENCES pages (page_id) MATCH SIMPLE ON UPDATE NO ACTION ON DELETE NO ACTION
 );
