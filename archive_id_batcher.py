0518e8979eaeb7a58d2ef2828276dab9b71c3eab
Paul Duke <mountainmanduke@gmail.com>
2020-11-30 15:57:36 -0500

    put constant in constant var

--- archive_id_batcher.py
+++ archive_id_batcher.py
@@ -5,10 +5,12 @@ import db_functions
 import config_utils
 
 DEFAULT_BATCH_SIZE = 1000
+DEFAULT_MIN_AD_CREATION_DATE = '2019-01-01'
 
 def main(config):
     country_code = config.get('SEARCH', 'COUNTRY_CODE', fallback=None)
-    min_ad_creation_date = config.get('SEARCH', 'MIN_AD_CREATION_DATE', fallback='2019-01-01')
+    min_ad_creation_date = config.get('SEARCH', 'MIN_AD_CREATION_DATE',
+                                      fallback=DEFAULT_MIN_AD_CREATION_DATE)
     with config_utils.get_database_connection_from_config(config) as database_connection:
         database_interface = db_functions.DBInterface(database_connection)
         database_interface.make_snapshot_fetch_batches(batch_size=DEFAULT_BATCH_SIZE,
