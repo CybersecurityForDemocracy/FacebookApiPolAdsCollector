b7d16686f60ed0416c29d8e54c05d43ff8c602f0
Paul Duke <mountainmanduke@gmail.com>
2020-11-30 15:47:31 -0500

    add configurable min ad creation date condition to archive id batcher

--- archive_id_batcher.py
+++ archive_id_batcher.py
@@ -8,10 +8,12 @@ DEFAULT_BATCH_SIZE = 1000
 
 def main(config):
     country_code = config.get('SEARCH', 'COUNTRY_CODE', fallback=None)
+    min_ad_creation_date = config.get('SEARCH', 'MIN_AD_CREATION_DATE', fallback='2019-01-01')
     with config_utils.get_database_connection_from_config(config) as database_connection:
         database_interface = db_functions.DBInterface(database_connection)
         database_interface.make_snapshot_fetch_batches(batch_size=DEFAULT_BATCH_SIZE,
-                                                       country_code=country_code)
+                                                       country_code=country_code,
+                                                       min_ad_creation_date=min_ad_creation_date)
 
 if __name__ == '__main__':
     config_utils.configure_logger('archive_id_batcher.log')
