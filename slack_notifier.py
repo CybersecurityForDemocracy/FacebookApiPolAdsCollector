5695b8431b83088788d7ddd60c50cc3143720263
Paul Duke <mountainmanduke@gmail.com>
2020-12-04 15:24:13 -0500

    copy slack_notifier from FacebookAdsAnalysis because they do the same thing and it reduces maintanence burden

--- slack_notifier.py
+++ slack_notifier.py
@@ -1,23 +1,35 @@
+"""Module and/or standalone script to sent messages to a given slack webhook URL"""
 import json
 import logging
+import sys
 
 import requests
 
-headers = {
+HEADERS = {
     'Content-type': 'application/json',
 }
 
 def notify_slack(devops_channel_url, message):
-    logging.info('Slack notification: %s', message)
-    if devops_channel_url:
-        data = json.dumps({'text': message})
-        requests.post(devops_channel_url, headers=headers, data=data)
-        return
-    else:
-        logging.warning('No Slack URL provided, logging locally only.')
-    # Curl request: 
-    # curl -X POST -H 'Content-type: application/json' --data '{"text":"Hello, World!"}' <SLACK URL FROM CONFIG FILE>
+    """Sends message to devops_channel_url slack webhook URL.
 
-# Quick and dirty test
-# if __name__ == '__main__':
-#     notify_slack('Test String with `fun\'ny" characters')
+    Args:
+        devops_channel_url: str; slack webhookd URL to send message to.
+        message: str; message to send
+    """
+    logging.info('Slack notification: %s', message)
+    if not devops_channel_url:
+        logging.warning('No Slack URL provided, logging locally only.')
+        return
+
+    data = json.dumps({'text': message})
+    requests.post(devops_channel_url, headers=HEADERS, data=data)
+
+# Curl request:
+# curl -X POST -H 'Content-type: application/json' --data '{"text":"Hello, World!"}' <SLACK URL>
+
+
+if __name__ == '__main__':
+    if len(sys.argv) < 3:
+        print('USAGE', sys.argv[0], ' <slack webhook URL> <message>')
+        sys.exit(1)
+    notify_slack(sys.argv[1], sys.argv[2])
