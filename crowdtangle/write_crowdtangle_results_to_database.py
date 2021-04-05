5854e7c868a3f9ce36b3ea0747aab1846993100b
Paul <mountainmanduke@gmail.com>
2021-04-05 19:51:30 -0400

    add table for dashboard names, associate posts to dashboards they were found from. infra to pass dashboard name in, get the ID, and update that info when writing to DB. (#189)

--- crowdtangle/write_crowdtangle_results_to_database.py
+++ crowdtangle/write_crowdtangle_results_to_database.py
@@ -27,14 +27,16 @@ class WriteCrowdTangleResultsToDatabase(beam.DoFn):
     """DoFn that expects iterables of process_crowdtangle_posts.EncapsulatedPost and writes the
     contained data to database (in order FK relationships reqire).
     """
-    def __init__(self, database_connection_params, max_batch_size=250):
+    def __init__(self, database_connection_params, dashboard_name, max_batch_size=250):
         self._database_connection_params = database_connection_params
         self._max_batch_size = max_batch_size
+        self._dashboard_name = dashboard_name
 
     def process(self, pcoll):
         database_connection = config_utils.get_database_connection(self._database_connection_params)
         with database_connection:
             db_interface = db_functions.CrowdTangleDBInterface(database_connection)
+
             db_interface.upsert_accounts(get_account_record_list_only_latest_updated_records(pcoll))
             db_interface.upsert_posts(itertools.chain(map(attrgetter('post'), pcoll)))
             db_interface.upsert_statistics(
@@ -44,3 +46,8 @@ class WriteCrowdTangleResultsToDatabase(beam.DoFn):
                     map(attrgetter('expanded_links'), pcoll)))
             db_interface.upsert_media(itertools.chain.from_iterable(
                     map(attrgetter('media_list'), pcoll)))
+            db_interface.insert_post_dashboards(
+                self._dashboard_name,
+                # Get all post objects' IDs
+                map(attrgetter('id'), itertools.chain(
+                    map(attrgetter('post'), pcoll))))
