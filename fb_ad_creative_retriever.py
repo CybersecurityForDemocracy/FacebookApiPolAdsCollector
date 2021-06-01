753833220d8bbaf7da4b3bdae03595c5e03aef8c
Paul <mountainmanduke@gmail.com>
2021-06-01 14:45:24 -0400

    fix syntax

--- fb_ad_creative_retriever.py
+++ fb_ad_creative_retriever.py
@@ -149,7 +149,7 @@ def upload_blob(bucket_client, blob_path, blob_data):
     blob = bucket_client.blob(blob_path)
     if blob.exists():
         logging.info('Blob path %s already exists, skipping upload', blob_path)
-    else
+    else:
         blob.upload_from_string(blob_data)
     return blob.id
 
