eae6dd341bd805283219809ae498f12aba7cedb2
Paul <mountainmanduke@gmail.com>
2021-06-04 13:03:23 -0400

    Handle multiple dashboards in FetchCrowdTangle PTransform, to config multiple dashboards add config var DASHBOARD_CONFIG_SECTION_NAMES which should be a comma separated list of config sections to read dashboard config data from

--- crowdtangle/fetch_crowdtangle.py
+++ crowdtangle/fetch_crowdtangle.py
@@ -7,7 +7,8 @@ from minet.crowdtangle import CrowdTangleAPIClient
 from minet.crowdtangle.exceptions import CrowdTangleError
 
 
-FetchCrowdTangleArgs = namedtuple('FetchCrowdTangleArgs', ['start_date',
+FetchCrowdTangleArgs = namedtuple('FetchCrowdTangleArgs', ['api_token',
+                                                           'start_date',
                                                            'end_date',
                                                            'list_ids',
                                                            'dashboard_id',
@@ -15,26 +16,30 @@ FetchCrowdTangleArgs = namedtuple('FetchCrowdTangleArgs', ['start_date',
 
 
 class FetchCrowdTangle(PTransform):
-    def __init__(self, *args, api_token=None, crowdtangle_client=None, **kwargs):
+    def __init__(self, *args, crowdtangle_client=None, **kwargs):
         super().__init__(*args, **kwargs)
-        if api_token and crowdtangle_client:
-            raise ValueError('api_token and crowdtangle_client args are mutually exclusive.')
-        self._api_token = api_token
+        #  if fetch_crowdtangle_args and crowdtangle_client:
+            #  raise ValueError('api_token and crowdtangle_client args are mutually exclusive.')
+        #  self._fetch_crowdtangle_args = fetch_crowdtangle_args
         self._crowdtangle_client = crowdtangle_client
 
-    def get_crowdtangle_client(self):
+    def get_crowdtangle_client(self, api_token=None):
         """Returns the CrowdTangleAPIClient provided in the constructor, or creates a new client
         from API token stores in GCP secrets manager.
 
         This is neccessary because CrowdTangleAPIClient hangs when pickled and then depickled (which
         Apache Beam does sometimes for PTransform objects)
         """
+        if api_token and self._crowdtangle_client:
+            raise ValueError('api_token and crowdtangle_client args are mutually exclusive.')
+
         if self._crowdtangle_client:
             return self._crowdtangle_client
 
-        return CrowdTangleAPIClient(token=self._api_token)
+        return CrowdTangleAPIClient(token=api_token)
 
     def fetch(self, input_args):
+        logging.info('in FetchCrowdTangle.fetch input_args: %s', input_args)
         try:
             start_date = input_args.start_date
         except KeyError as e:
@@ -59,7 +64,7 @@ class FetchCrowdTangle(PTransform):
         logging.info('Querying CrowdTangle. %s', query_info_message)
         num_posts = 0
         try:
-            crowdtangle_client = self.get_crowdtangle_client()
+            crowdtangle_client = self.get_crowdtangle_client(input_args.api_token)
             for post in crowdtangle_client.posts(start_date=start_date, end_date=end_date,
                                                  partition_strategy=partition_strategy,
                                                  sort_by=sort_by, format=format_val,
--- crowdtangle/run_fetch_crowdtangle.py
+++ crowdtangle/run_fetch_crowdtangle.py
@@ -1,6 +1,9 @@
 import argparse
+import configparser
 import datetime
 import logging
+
+from typing import Sequence
 
 import apache_beam as beam
 from apache_beam.options.pipeline_options import PipelineOptions
@@ -12,6 +15,47 @@ from crowdtangle import write_crowdtangle_results_to_database
 
 import config_utils
 from crowdtangle import db_functions
+
+def get_dashboards_fetch_args(config: configparser.ConfigParser) -> Sequence[fetch_crowdtangle.FetchCrowdTangleArgs]:
+    """Gets list of config section names from ['CROWDTANGLE']['DASHBOARD_CONFIG_SECTION_NAMES'],
+    parses API_TOKEN, DASHBOARD_NAME, LIST_IDS from those named config sections, and returns
+    FetchCrowdTangleArgs for each named config section.
+    """
+    dashboard_config_section_names = (
+        config['CROWDTANGLE']['DASHBOARD_CONFIG_SECTION_NAMES'].split(','))
+
+    database_connection_params = config_utils.get_database_connection_params_from_config(config)
+    with config_utils.get_database_connection(database_connection_params) as db_connection:
+        db_interface = db_functions.CrowdTangleDBInterface(db_connection)
+        dashboard_name_to_id = db_interface.all_dashboards_name_to_id()
+        logging.info('Dashboard Names -> IDs: %s', dashboard_name_to_id)
+
+    fetch_args_list = []
+    for config_section_name in dashboard_config_section_names:
+        max_results_to_fetch = config['CROWDTANGLE'].getint('MAX_RESULTS_TO_FETCH', None)
+        if 'DAYS_IN_PAST_TO_SYNC' in config['CROWDTANGLE']:
+            start_date = (datetime.date.today() -
+                          datetime.timedelta(days=config['CROWDTANGLE'].getint('DAYS_IN_PAST_TO_SYNC'))
+                          ).isoformat()
+            end_date = None
+        else:
+            start_date = config['CROWDTANGLE'].get('START_DATE')
+            end_date = config['CROWDTANGLE'].get('END_DATE', None)
+        api_token = config[config_section_name].get('API_TOKEN')
+        dashboard_name = config[config_section_name].get('DASHBOARD_NAME')
+        list_ids = config[config_section_name].get('LIST_IDS', None)
+        if list_ids:
+            list_ids = list_ids.split(',')
+
+        fetch_args_list.append(fetch_crowdtangle.FetchCrowdTangleArgs(
+                    api_token=api_token,
+                    list_ids=list_ids,
+                    start_date=start_date,
+                    end_date=end_date,
+                    dashboard_id=dashboard_name_to_id[dashboard_name],
+                    max_results_to_fetch=max_results_to_fetch))
+    return fetch_args_list
+
 
 def run(argv=None, save_main_session=True):
     """Main entry point; defines and runs the wordcount pipeline."""
@@ -29,38 +73,43 @@ def run(argv=None, save_main_session=True):
     pipeline_options.view_as(SetupOptions).save_main_session = save_main_session
 
     config = config_utils.get_config(known_args.config_path)
-    max_results_to_fetch = config['CROWDTANGLE'].getint('MAX_RESULTS_TO_FETCH', None)
-    if 'DAYS_IN_PAST_TO_SYNC' in config['CROWDTANGLE']:
-        start_date = (datetime.date.today() -
-                      datetime.timedelta(days=config['CROWDTANGLE'].getint('DAYS_IN_PAST_TO_SYNC'))
-                      ).isoformat()
-        end_date = None
-    else:
-        start_date = config['CROWDTANGLE'].get('START_DATE')
-        end_date = config['CROWDTANGLE'].get('END_DATE', None)
-    api_token = config['CROWDTANGLE'].get('API_TOKEN')
-    list_ids = config['CROWDTANGLE'].get('LIST_IDS', None)
-    dashboard_name = config['CROWDTANGLE'].get('DASHBOARD_NAME')
-    if list_ids:
-        list_ids = list_ids.split(',')
+    #  max_results_to_fetch = config['CROWDTANGLE'].getint('MAX_RESULTS_TO_FETCH', None)
+    #  if 'DAYS_IN_PAST_TO_SYNC' in config['CROWDTANGLE']:
+        #  start_date = (datetime.date.today() -
+                      #  datetime.timedelta(days=config['CROWDTANGLE'].getint('DAYS_IN_PAST_TO_SYNC'))
+                      #  ).isoformat()
+        #  end_date = None
+    #  else:
+        #  start_date = config['CROWDTANGLE'].get('START_DATE')
+        #  end_date = config['CROWDTANGLE'].get('END_DATE', None)
+    #  api_token = config['CROWDTANGLE'].get('API_TOKEN')
+    #  list_ids = config['CROWDTANGLE'].get('LIST_IDS', None)
+    #  dashboard_name = config['CROWDTANGLE'].get('DASHBOARD_NAME')
+    #  if list_ids:
+        #  list_ids = list_ids.split(',')
 
-    database_connection_params = config_utils.get_database_connection_params_from_config(config)
-    with config_utils.get_database_connection(database_connection_params) as db_connection:
-        db_interface = db_functions.CrowdTangleDBInterface(db_connection)
-        dashboard_name_to_id = db_interface.all_dashboards_name_to_id()
-        logging.info('Dashboard Names -> IDs: %s', dashboard_name_to_id)
+    #  database_connection_params = config_utils.get_database_connection_params_from_config(config)
+    #  with config_utils.get_database_connection(database_connection_params) as db_connection:
+        #  db_interface = db_functions.CrowdTangleDBInterface(db_connection)
+        #  dashboard_name_to_id = db_interface.all_dashboards_name_to_id()
+        #  logging.info('Dashboard Names -> IDs: %s', dashboard_name_to_id)
 
-    fetch_crowdtangle_args = fetch_crowdtangle.FetchCrowdTangleArgs(
-                list_ids=list_ids,
-                start_date=start_date,
-                end_date=end_date,
-                dashboard_id=dashboard_name_to_id[dashboard_name],
-                max_results_to_fetch=max_results_to_fetch)
+    #  fetch_crowdtangle_args = fetch_crowdtangle.FetchCrowdTangleArgs(
+                #  list_ids=list_ids,
+                #  start_date=start_date,
+                #  end_date=end_date,
+                #  dashboard_id=dashboard_name_to_id[dashboard_name],
+                #  max_results_to_fetch=max_results_to_fetch)
+    fetch_args_list = get_dashboards_fetch_args(config)
 
     logging.info('About to start crowdtangle fetch pipline with args: %s', fetch_crowdtangle_args)
     with beam.Pipeline(options=pipeline_options) as pipeline:
+        dashboard_crowdtangle_fetchers = []
+        for fetch_args in fetch_args_list:
+            dashboard_crowdtangle_fetchers.append(fetch_crowdtangle.FetchCrowdTangle(fetch_args))
+
         results, errors = (
-            pipeline | beam.Create([fetch_crowdtangle_args])
+            pipeline | beam.Create(fetch_args_list)
             | 'Fetch CrowdTangle results' >> fetch_crowdtangle.FetchCrowdTangle(api_token=api_token)
             )
 
