aa9da110bb908beba8d5a987044a2214267f2389
Paul <mountainmanduke@gmail.com>
2021-06-15 11:24:58 -0400

    upgrade minet version to 0.52.10, and refactor code to handle interface/datasturct changes (#202)
    * get map of dashboard name -> id before pipeline start. attached dashboard id to encapsulated post instead of doing dashboard name -> id lookup in insert_post_dashboards
    
    * fix crowdtangle db_functions import
    
    * log Dashboard Names -> IDs
    
    * write_crowdtangle_results_to_database.py from master
    
    * post merge cleanup
    
    * Handle multiple dashboards in FetchCrowdTangle PTransform, to config multiple dashboards add config var DASHBOARD_CONFIG_SECTION_NAMES which should be a comma separated list of config sections to read dashboard config data from
    
    * var referene and other lint cleanups
    
    * bump minet dep version due to second-level dep issue (quenouille)
    
    * fix minet version
    
    * minet now returns objects as casanova.namedrecord, so convert it to dict before adding annotation.
    
    * Lots of changes to handle new post datastructure format from minet v0.52.8 CrowdTangleAPIClient
    
    * fix keyword arg for ExpandedLinkRecord
    
    * add necessary PostRecord namedtuple arg
    
    * fix make_statistics_record keyword arg
    
    * fix arg reference in make_statistics_record
    
    * import itertools
    
    * add default False for account_verified
    
    * check media and links values instead of just key presence
    
    * log ProcessCrowdTanglePosts.process input args for debugging
    
    * move crowdtangle fetch out of PTransform into own DoFn
    
    * move crowdtangle fetch out of PTransform into own DoFn
    
    * see if parallelism works with FlatMap
    
    * undo accidental uncapitalization of code block
    
    * remove unneeded code
    
    * remove more unneeded code
    
    * move date parsing out of dashboard parsing loop, move max_results_to_fetch to be dashboard_specific, simplify ref to config_section with new var instead of lookup each time
    
    * more cleanup
    
    * pin minet version to 0.52, but allow latest subversion
    
    * remove more unused/old code
    
    * up minet minor version

--- crowdtangle/process_crowdtangle_posts.py
+++ crowdtangle/process_crowdtangle_posts.py
@@ -1,12 +1,14 @@
 from collections import namedtuple
 import datetime
+import itertools
 
 import apache_beam as beam
 
-_ID_KEY = 'id'
+_ID_KEY = 'ct_id'
 _STATISTICS_KEY = 'statistics'
 _MEDIA_KEY = 'media'
-_EXPANDED_LINKS_KEY = 'expandedLinks'
+_ORIGINAL_LINKS_KEY = 'links'
+_EXPANDED_LINKS_KEY = 'expanded_links'
 _BRANDED_CONTENT_SPONSOR = 'brandedContentSponsor'
 
 EncapsulatedPost = namedtuple('EncapsulatedPost',
@@ -85,38 +87,38 @@ class ProcessCrowdTanglePosts(beam.DoFn):
     """Accepts dict representation of crowdtangle post object, and transforms it EncapsulatedPost.
     """
     @staticmethod
-    def make_account_record(account, updated):
+    def make_account_record(item, updated):
         return AccountRecord(
-            id=account['id'],
-            account_type=account.get('accountType'),
-            handle=account.get('handle'),
-            name=account.get('name'),
-            page_admin_top_country=account.get('pageAdminTopCountry'),
-            platform=account.get('platform'),
-            platform_id=account.get('platformId'),
-            profile_image=account.get('profileImage'),
-            subscriber_count=account.get('subscriberCount'),
-            url=account.get('url'),
-            verified=account.get('verified'),
+            id=item['account_ct_id'],
+            account_type=item.get('account_type'),
+            handle=item.get('account_handle'),
+            name=item.get('account_name'),
+            page_admin_top_country=item.get('account_page_admin_top_country'),
+            platform=item.get('platform'),
+            platform_id=item.get('account_id'),
+            profile_image=item.get('account_profile_image'),
+            subscriber_count=item.get('account_subscriber_count'),
+            url=item.get('accout_url'),
+            verified=item.get('accout_verified', False),
             updated=updated)
 
     @staticmethod
-    def make_statistics_record(statistics, post_id, updated):
+    def make_statistics_record(statistics, key_prefix, post_id, updated):
         return StatisticsRecord(
             post_id=post_id,
             updated=updated,
-            angry_count=statistics.get('angryCount'),
-            comment_count=statistics.get('commentCount'),
-            favorite_count=statistics.get('favoriteCount'),
-            haha_count=statistics.get('hahaCount'),
-            like_count=statistics.get('likeCount'),
-            love_count=statistics.get('loveCount'),
-            sad_count=statistics.get('sadCount'),
-            share_count=statistics.get('shareCount'),
-            up_count=statistics.get('upCount'),
-            wow_count=statistics.get('wowCount'),
-            thankful_count=statistics.get('thankfulCount'),
-            care_count=statistics.get('careCount'))
+            angry_count=statistics.get(key_prefix + 'angry_count'),
+            comment_count=statistics.get(key_prefix + 'comment_count'),
+            favorite_count=statistics.get(key_prefix + 'favorite_count'),
+            haha_count=statistics.get(key_prefix + 'haha_count'),
+            like_count=statistics.get(key_prefix + 'like_count'),
+            love_count=statistics.get(key_prefix + 'love_count'),
+            sad_count=statistics.get(key_prefix + 'sad_count'),
+            share_count=statistics.get(key_prefix + 'share_count'),
+            up_count=statistics.get(key_prefix + 'up_count'),
+            wow_count=statistics.get(key_prefix + 'wow_count'),
+            thankful_count=statistics.get(key_prefix + 'thankful_count'),
+            care_count=statistics.get(key_prefix + 'care_count'))
 
     def process(self, item):
         post_id = item[_ID_KEY]
@@ -125,47 +127,39 @@ class ProcessCrowdTanglePosts(beam.DoFn):
 
         post_record = PostRecord(
             id=post_id,
-            account_id=item['account']['id'],
-            branded_content_sponsor_account_id=item.get('brandedContentSponsor', {}).get('id'),
+            account_id=item['account_ct_id'],
+            # TODO(macpd): figure out how new minet CrowdTangleAPIClient handles
+            # brandeeContentSponsor
+            #  branded_content_sponsor_account_id=item.get('branded_content_sponsor', {}).get('ct_id'),
+            branded_content_sponsor_account_id=None,
             message=item.get('message'),
             title=item.get('title'),
             platform=item.get('platform'),
-            platform_id=item.get('platformId'),
-            post_url=item.get('postUrl'),
-            subscriber_count=item.get('subscriberCount'),
+            platform_id=item.get('id'),
+            post_url=item.get('post_url'),
+            subscriber_count=item.get('subscriber_count'),
             type=item.get('type'),
             updated=post_updated,
-            video_length_ms=item.get('videoLengthMs'),
-            image_text=item.get('imageText'),
-            legacy_id=item.get('legacyId'),
+            video_length_ms=item.get('video_length_ms'),
+            image_text=item.get('image_text'),
+            legacy_id=item.get('legacy_id'),
             caption=item.get('caption'),
             link=item.get('link'),
             date=item.get('date'),
             description=item.get('description'),
             score=item.get('score'),
-            live_video_status=item.get('liveVideoStatus'),
-            language_code=item.get('languageCode'))
+            live_video_status=item.get('live_video_status'),
+            language_code=item.get('language_code'))
 
-        account_list = [self.make_account_record(item['account'], post_updated)]
+        account_list = [self.make_account_record(item, post_updated)]
 
-        if _BRANDED_CONTENT_SPONSOR in item:
-            account_list.append(self.make_account_record(item[_BRANDED_CONTENT_SPONSOR],
-                                                         post_updated))
-
-        statistics_actual = None
-        statistics_expected = None
-        if _STATISTICS_KEY in item:
-            statistics = item[_STATISTICS_KEY]
-            if 'actual' in statistics:
-                statistics_actual = self.make_statistics_record(
-                    item[_STATISTICS_KEY]['actual'], post_id=post_id, updated=post_updated)
-            if 'expected' in statistics:
-                statistics_expected = self.make_statistics_record(
-                    item[_STATISTICS_KEY]['expected'], post_id=post_id,
-                    updated=post_updated)
+        statistics_actual = self.make_statistics_record(
+            item, key_prefix='actual', post_id=post_id, updated=post_updated)
+        statistics_expected = self.make_statistics_record(
+            item, key_prefix='expected', post_id=post_id, updated=post_updated)
 
         media_records = []
-        if _MEDIA_KEY in item:
+        if item.get(_MEDIA_KEY):
             media_records = [
                 MediaRecord(post_id=post_id, updated=post_updated, url_full=medium.get('full'),
                          url=medium.get('url'), width=medium.get('width'),
@@ -173,11 +167,13 @@ class ProcessCrowdTanglePosts(beam.DoFn):
                 for medium in item[_MEDIA_KEY]]
 
         expanded_links = []
-        if _EXPANDED_LINKS_KEY in item:
+        if item.get(_ORIGINAL_LINKS_KEY):
+            zipped_links_iter = itertools.zip_longest(item[_ORIGINAL_LINKS_KEY],
+                                                      item.get(_EXPANDED_LINKS_KEY, []))
             expanded_links = [
                 ExpandedLinkRecord(post_id=post_id, updated=post_updated,
-                                   original=link.get('original'), expanded=link.get('expanded'))
-                for link in item[_EXPANDED_LINKS_KEY]]
+                                   original=original_link, expanded=expanded_link)
+                for original_link, expanded_link in zipped_links_iter]
 
         yield EncapsulatedPost(post=post_record, account_list=account_list,
                                statistics_actual=statistics_actual,
