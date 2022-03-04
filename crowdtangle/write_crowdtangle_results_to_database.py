b8049b6109de2603c03631c1cbf0f6a041f8efa3
Paul <mountainmanduke@gmail.com>
2022-03-04 14:49:19 -0500

    dedupe posts, before writing to DB, with same ID by max updated (ie only most recent version of post) (#218)
    * dedupe posts, before writing to DB, with same ID by max updated (ie only most recent version of post)
    
    * consolidate similar logic into single function

--- crowdtangle/write_crowdtangle_results_to_database.py
+++ crowdtangle/write_crowdtangle_results_to_database.py
@@ -10,23 +10,28 @@ from crowdtangle import db_functions
 
 logger = logging.getLogger()
 
-def dedupe_account_records_by_max_updated_field(account_records):
-    """Return list of account records deduped by ID. If multiple records with the same ID are found
-    the record with the highest/latest |updated| is returned.
+def dedupe_records_with_same_id_by_max_updated_field(records):
+    """Return list of records deduped by ID. If multiple records with the same ID are found the
+    record with the highest/latest |updated| is returned.
     """
-    account_id_to_latest_updated_record = {}
-    for account in account_records:
-        if account.id in account_id_to_latest_updated_record:
-            account_id_to_latest_updated_record[account.id] = max(
-                account, account_id_to_latest_updated_record[account.id], key=attrgetter('updated'))
+    record_id_to_latest_updated_record = {}
+    for record in records:
+        if record.id in record_id_to_latest_updated_record:
+            record_id_to_latest_updated_record[record.id] = max(
+                record, record_id_to_latest_updated_record[record.id], key=attrgetter('updated'))
         else:
-            account_id_to_latest_updated_record[account.id] = account
-    return list(account_id_to_latest_updated_record.values())
+            record_id_to_latest_updated_record[record.id] = record
+    return list(record_id_to_latest_updated_record.values())
 
 def get_account_record_list_only_latest_updated_records(pcoll):
     """Returns list of account records deduped by max updated field."""
-    return dedupe_account_records_by_max_updated_field(
+    return dedupe_records_with_same_id_by_max_updated_field(
         itertools.chain.from_iterable(map(attrgetter('account_list'), pcoll)))
+
+def get_post_record_list_only_latest_updated_records(pcoll):
+    """Returns list of post records deduped by max updated field."""
+    return dedupe_records_with_same_id_by_max_updated_field(
+        itertools.chain(map(attrgetter('post'), pcoll)))
 
 class WriteCrowdTangleResultsToDatabase(beam.DoFn):
     """DoFn that expects iterables of process_crowdtangle_posts.EncapsulatedPost and writes the
@@ -47,7 +52,7 @@ class WriteCrowdTangleResultsToDatabase(beam.DoFn):
             db_interface = db_functions.CrowdTangleDBInterface(database_connection)
 
             db_interface.upsert_accounts(get_account_record_list_only_latest_updated_records(pcoll))
-            db_interface.upsert_posts(itertools.chain(map(attrgetter('post'), pcoll)))
+            db_interface.upsert_posts(get_post_record_list_only_latest_updated_records(pcoll))
             db_interface.upsert_statistics(
                 itertools.chain(map(attrgetter('statistics_actual'), pcoll)),
                 itertools.chain(map(attrgetter('statistics_expected'), pcoll)))
